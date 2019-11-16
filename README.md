1934b832142ddcdb3f99721ef5d414dd7d9b738f
tmaurhart <thomas.maurhart@gmail.com>
2019-11-16 12:02:39 +0100

    upload

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # maschinelearning-medientechnik
+
+#start this project
+
+<ul>
+    <li>npm install</li>
+    <li>npm run test</li>
+    <li>choose folder <i>example-part1</i> or <i>example-part2</i></li>
+</ul>
---
+++ examples-part2/image_detection/sketch.js
@@ -0,0 +1,62 @@
+let classifier;
+
+let img;
+let currentIndex = 0;
+let allImages = [];
+let predictions = [];
+
+function preload() {
+  classifier = ml5.imageClassifier('MobileNet');
+  data = loadJSON('assets/data.json');
+}
+
+function setup() {
+  createCanvas(400, 400);
+  background(0);
+  appendImages();
+  loadImage(allImages[currentIndex], imageReady);
+}
+
+function appendImages() {
+  for (i = 0; i < data.images.length; i++) {
+    imgPath = data.images[i];
+    allImages.push('images/dataset/' + imgPath);
+  }
+}
+
+// When the image has been loaded,
+// get a prediction for that image
+function imageReady(img) {
+  image(img, 0, 0);
+  classifier.classify(img, gotResult);
+}
+
+function savePredictions() {
+  predictionsJSON = {
+    predictions: predictions
+  };
+  saveJSON(predictionsJSON, 'predictions.json');
+}
+
+// When we get the results
+function gotResult(err, results) {
+  // If there is an error, show in the console
+  if (err) {
+    console.error(err);
+  }
+
+  information = {
+    name: allImages[currentIndex],
+    result: results
+  };
+
+  predictions.push(information);
+  createDiv('Label: ' + results[0].label);
+  createDiv('Confidence: ' + nf(results[0].confidence, 0, 2));
+  currentIndex++;
+  if (currentIndex <= allImages.length - 1) {
+    loadImage(allImages[currentIndex], imageReady);
+  } else {
+    savePredictions();
+  }
+}
